33f580abde7e7779a4c71979a6f926cc643749db
Harlan Cleiton <harlancleiton@gmail.com>
2018-02-18 19:47:33 -0300

    Entendendo como funciona Observable do Angular. Integração com Travis CI.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/harlancleiton/PassaroUrbano.svg?branch=master)](https://travis-ci.org/harlancleiton/PassaroUrbano)
+
 # PassaroUrbano
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.8.
--- src/app/offer/offer.component.ts
+++ src/app/offer/offer.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OffersServices } from '../offers.services'
 import { OfferModel } from '../shared/offer.model'
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/Rx'
+import { Observer } from 'rxjs/Rx';
 
 @Component({
   selector: 'pu-offer',
@@ -16,10 +19,29 @@ export class OfferComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute, private offersServices: OffersServices) { }
 
   ngOnInit() {
+    //Observado
+    let observado = Observable.create((observer: Observer<string>) => {
+        observer.next('Primeiro evento da stream')
+        observer.next('Segundo evento da stream')
+        observer.complete()
+        observer.error('Error')
+        observer.next('Terceiro evento da stream')
+    })
+    //Observador
+    observado.subscribe(
+      (param: any) => console.log(param),
+      (error: string) => console.log(error),
+      () => console.log('Sucesso')
+    )
+
+    /*
+    let time = Observable.interval(3000)
+    time.subscribe((interval: number) => console.log(interval))
     this.offersServices.getOffersById(this.activatedRoute.snapshot.params['id'])
       .then((offerModel: OfferModel) => {
         this.offerModel = offerModel
       })
+      */
     /*
   //Subscribe fica assistindo alterações na rota (Observable)
   this.activatedRoute.params.subscribe(
