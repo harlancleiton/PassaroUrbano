77dd61ebc327d65dff6b50c150134e0afd689256
Harlan Cleiton <harlancleiton@gmail.com>
2018-05-17 23:34:42 -0300

    Utilizando Two-Way-Binding para tratamento de formulários

--- src/app/purchase-order/purchase-order.component.ts
+++ src/app/purchase-order/purchase-order.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PurchaseOrderComponent implements OnInit {
 
+  public address: string = 'Rua ABC'
+  public number: number = 0
+  public complement: string = 'Av 00'
+  public payment: string = 'money'
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public updateAddress(address: string): void {
+    this.address = address
+    console.log(this.address)
+  }
+
+  public updateNumber(number: number): void {
+    this.number = number
+    console.log(this.number)
+  }
+
+  public updateComplement(complement: string): void {
+    this.complement = complement
+    console.log(this.complement)
+  }
+
+  public changePayment (payment: string) {
+    this.payment = payment
+    console.log(this.payment)
+  }
 }
